4bf51dcdc2c9d7765dbb7eeb2043a362d40f893a
Saad Attia <saadattia99@gmail.com>
2025-07-20 20:06:50 +0300

    sql_query_p1_Pgadmin4.sql

--- sql_query_p1_Pgadmin4.sql
+++ sql_query_p1_Pgadmin4.sql
@@ -95,8 +95,6 @@ FROM
 -- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
 -- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
--- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
-
 
 -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
 SELECT 
